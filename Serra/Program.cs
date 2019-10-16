e991287246b3c2de8d57bc0bd8b8f9fbea827076
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:08:54 +0200

    Add project files.

---
+++ Serra/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Serra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
