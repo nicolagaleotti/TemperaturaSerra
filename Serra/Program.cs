54b08be6f7329c2672bf0427e22718a3600fef0b
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:17:48 +0200

    fine programma

--- Serra/Program.cs
+++ Serra/Program.cs
@@ -6,7 +6,21 @@ namespace Serra
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Inserisci la temperatura della serra: ");
+            double temp = double.Parse(Console.ReadLine());
+            if (temp < 5)
+            {
+                Console.WriteLine("Danni irreparabili");
+            }
+            else if (5 <= temp && temp < 18)
+            {
+                Console.WriteLine("Situazione di pericolo");
+            }
+            else
+            {
+                Console.WriteLine("Temperatura normale");
+            }
+            Console.ReadLine();
         }
     }
 }
